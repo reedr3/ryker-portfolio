f020df5fe74f6d79a552dce1aeb25fb4128cf688
Ryker Reed <ryker.reed3@gmail.com>
2017-06-12 15:50:43 -0700

    Modifies block component and adds css for alternating left and right justification of images and text within each page of blocks.

--- app/assets/javascripts/components/block.js.jsx
+++ app/assets/javascripts/components/block.js.jsx
@@ -4,11 +4,18 @@ var Block = React.createClass({
   },
 
   render: function() {
+    var textAlignment = {textAlign: 'left'};
+    var imgAlignment = {float: 'left'};
+    if (this.state.block.id % 2 == 0) {
+      textAlignment = {textAlign: 'right'};
+      imgAlignment = {float: 'right'};
+    }
+
     return (
       <div id="block">
-        <p>{this.state.block.header}</p>
-        <p>{this.state.block.image}</p>
-        <p>{this.state.block.description}</p>
+        <p style={textAlignment}>{this.state.block.header}</p>
+        <img style={imgAlignment} src={this.state.block.image} />
+        <p style={textAlignment}>{this.state.block.description}</p>
       </div>
     );
   }
