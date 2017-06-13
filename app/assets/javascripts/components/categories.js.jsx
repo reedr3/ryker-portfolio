37d07134ca157a1091a3a71379885781320bcbd2
Ryker Reed <ryker.reed3@gmail.com>
2017-06-13 14:32:46 -0700

    Refactors social media icons as own component.

--- app/assets/javascripts/components/categories.js.jsx
+++ app/assets/javascripts/components/categories.js.jsx
@@ -11,8 +11,9 @@ var Categories = React.createClass({
               return <li> <a href={"/categories/" + category.id}> {category.title} </a> </li>
             }.bind(this))}
         </ul>
-        <p>github and linkedin and fb etc links go here</p>
-        <a href="https://www.facebook.com/rebecca.reed.5667" className="fa fa-facebook"></a>
+        <Icon href="https://www.linkedin.com/in/ryker-reed/" className="fa fa-linkedin"/>
+        <Icon href="https://github.com/reedr3" className="fa fa-github"/>
+        <Icon href="https://www.facebook.com/rebecca.reed.5667" className="fa fa-facebook"/>
       </div>
     );
   }
