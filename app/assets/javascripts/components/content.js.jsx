f4b8ffda9212f9d74505be06114d2449c7ae0099
Ryker Reed <ryker.reed3@gmail.com>
2017-06-16 15:53:53 -0700

    Removes page component container. Designs basic css styling for header and footer components.

--- app/assets/javascripts/components/content.js.jsx
+++ app/assets/javascripts/components/content.js.jsx
@@ -1,7 +1,18 @@
 var Content = React.createClass({
   render: function() {
     return (
-      <h1>content</h1>
+      <main id="content">
+        <div className="container">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam consequatur molestiae tempora nulla aut iste maxime optio veritatis quod facilis similique ipsum sapiente facere impedit necessitatibus vero commodi ex delectus.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam consequatur molestiae tempora nulla aut iste maxime optio veritatis quod facilis similique ipsum sapiente facere impedit necessitatibus vero commodi ex delectus.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam consequatur molestiae tempora nulla aut iste maxime optio veritatis quod facilis similique ipsum sapiente facere impedit necessitatibus vero commodi ex delectus.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam consequatur molestiae tempora nulla aut iste maxime optio veritatis quod facilis similique ipsum sapiente facere impedit necessitatibus vero commodi ex delectus.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam consequatur molestiae tempora nulla aut iste maxime optio veritatis quod facilis similique ipsum sapiente facere impedit necessitatibus vero commodi ex delectus.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam consequatur molestiae tempora nulla aut iste maxime optio veritatis quod facilis similique ipsum sapiente facere impedit necessitatibus vero commodi ex delectus.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam consequatur molestiae tempora nulla aut iste maxime optio veritatis quod facilis similique ipsum sapiente facere impedit necessitatibus vero commodi ex delectus.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam consequatur molestiae tempora nulla aut iste maxime optio veritatis quod facilis similique ipsum sapiente facere impedit necessitatibus vero commodi ex delectus.</p>
+        </div>
+      </main>
     );
   }
 });
--- app/assets/javascripts/components/header.js.jsx
+++ app/assets/javascripts/components/header.js.jsx
@@ -1,7 +1,20 @@
 var Header = React.createClass({
   render: function() {
     return (
-      <h1>header</h1>
+      <header id="header">
+        <div className="container">
+          <img id="logo" src="https://s-media-cache-ak0.pinimg.com/736x/51/a6/a7/51a6a7af68bf9dd792645ceb248ebd68.jpg" />
+          <nav id="nav">
+            <ul>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">About</a></li>
+              <li><a href="#">Services</a></li>
+              <li><a href="#">History</a></li>
+              <li><a href="#">Contact</a></li>
+            </ul>
+          </nav>
+        </div>
+      </header>
     );
   }
 });
