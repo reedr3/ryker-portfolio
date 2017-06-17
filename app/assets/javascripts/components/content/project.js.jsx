9867407d5c1f26ccb9ea4c6bb1cfcec0cce72408
Ryker Reed <ryker.reed3@gmail.com>
2017-06-17 00:32:07 -0700

    Adds styling formatting for projects components. Starts adding content for projects.

--- app/assets/javascripts/components/content/project.js.jsx
+++ app/assets/javascripts/components/content/project.js.jsx
@@ -1,7 +1,15 @@
 var Project = React.createClass({
   render: function() {
     return (
-      <h1>I'm a project!</h1>
+      <div id="project">
+        <h1> {this.props.title} </h1>
+        <img src={this.props.image} />
+        <ul>
+          <li> <a href={this.props.github}> Github </a> </li>
+          <li> <a href={this.props.liveApp}> {this.props.liveAppName} </a> </li>
+        </ul>
+        <p> {this.props.description} </p>
+      </div>
     );
   }
 });
--- app/assets/javascripts/components/footer.js.jsx
+++ app/assets/javascripts/components/footer.js.jsx
@@ -6,7 +6,7 @@ var Footer = React.createClass({
           <Icon href="https://www.linkedin.com/in/ryker-reed/" className="fa fa-linkedin"/>
           <Icon href="https://github.com/reedr3" className="fa fa-github"/>
           <Icon href="https://www.facebook.com/rebecca.reed.5667" className="fa fa-facebook"/>
-          <p>Copyright Ryker 2017</p>
+          <p>Copyright 2017 Ryker Reed</p>
         </div>
       </footer>
     );
