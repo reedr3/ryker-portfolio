da0997ad507479c6741f8a88c98757f59759d048
Ryker Reed <ryker.reed3@gmail.com>
2017-06-19 17:31:25 -0700

    Refactors styling on projects page. Adds modal image popouts for each project. Makes each project more streamlined and clean.

--- app/assets/javascripts/components/content/hobby.js.jsx
+++ app/assets/javascripts/components/content/hobby.js.jsx
@@ -19,12 +19,12 @@ var Hobby = React.createClass({
     return (
       <div>
         <h1>I'm a hobby!</h1>
-        <img id="myImg" onClick={this.showModal} src="https://s-media-cache-ak0.pinimg.com/736x/51/a6/a7/51a6a7af68bf9dd792645ceb248ebd68.jpg" alt="test image for modal test" width="300" height="200" />
+        <img className="myImg" onClick={this.showModal} src="https://s-media-cache-ak0.pinimg.com/736x/51/a6/a7/51a6a7af68bf9dd792645ceb248ebd68.jpg" alt="test image for modal test" />
 
-        <div id="myModal" className="modal" style={this.state.modalDisplay}>
-          <span className="close" onClick={this.closeModal}>I'm the freaking close button</span>
-          <img className="modal-content" id="img01" src="https://s-media-cache-ak0.pinimg.com/736x/51/a6/a7/51a6a7af68bf9dd792645ceb248ebd68.jpg"/>
-          <div id="caption">This is a caption</div>
+        <div className="modal" style={this.state.modalDisplay}>
+          <span className="close" onClick={this.closeModal}>&times;</span>
+          <img className="modal-content" src="https://s-media-cache-ak0.pinimg.com/736x/51/a6/a7/51a6a7af68bf9dd792645ceb248ebd68.jpg"/>
+          <div className="caption">This is a caption</div>
         </div>
       </div>
     );
--- app/assets/javascripts/components/content/project.js.jsx
+++ app/assets/javascripts/components/content/project.js.jsx
@@ -1,14 +1,41 @@
 var Project = React.createClass({
+  getInitialState: function() {
+    return {modalDisplay: {display: "none"}};
+  },
+
+  showModal: function() {
+    var newState = this.state;
+    newState.modalDisplay = {display: "block"};
+    this.setState(newState);
+  },
+
+  closeModal: function() {
+    var newState = this.state;
+    newState.modalDisplay = {display: "none"};
+    this.setState(newState);
+  },
+
   render: function() {
     return (
       <div id="project">
         <h1> {this.props.title} </h1>
-        <img src={this.props.image} />
-        <ul>
-          <li> <a href={this.props.github}> Github Repo</a> </li>
-          <li> <a href={this.props.liveApp}> {this.props.liveAppName} </a> </li>
-        </ul>
-        <p> {this.props.description} </p>
+        <img className="myImg" onClick={this.showModal} src={this.props.image} />
+        <div className="external-links">
+          <a href={this.props.github}> Github Repo</a>
+          <a href={this.props.liveApp}> {this.props.liveAppName} </a>
+        </div>
+
+        <div className="modal" style={this.state.modalDisplay}>
+          <span className="close" onClick={this.closeModal}>&times;</span>
+          <h2> {this.props.title} </h2>
+          <img className="modal-content" src={this.props.image} />
+          <div className="caption"> {this.props.description} </div>
+            <ul>
+              <li> React </li>
+              <li> Rails </li>
+              <li> Ruby </li>
+            </ul>
+        </div>
       </div>
     );
   }
