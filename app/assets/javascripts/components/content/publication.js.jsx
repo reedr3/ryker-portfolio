26cfc1830a41f5b30c3a59784a3951b65177d635
Ryker Reed <ryker.reed3@gmail.com>
2017-06-20 15:23:44 -0700

    Adds styling and content for publication and publication modal components.

--- app/assets/javascripts/components/content/publication.js.jsx
+++ app/assets/javascripts/components/content/publication.js.jsx
@@ -1,7 +1,33 @@
 var Publication = React.createClass({
+  getInitialState: function() {
+    return {modalDisplay: {display: "none"}};
+  },
+
+  showModal: function() {
+    var newState = this.state;
+    newState.modalDisplay = {display: "block"};
+    this.setState(newState);
+  },
+
+  closeModal: function() {
+    var newState = this.state;
+    newState.modalDisplay = {display: "none"};
+    this.setState(newState);
+  },
+
   render: function() {
     return (
-      <h1>I'm a publication!</h1>
+      <div className="publication">
+        <h1> {this.props.title} </h1>
+        <ImageWithModal showModalCallback={this.showModal} image={this.props.image} />
+        <div className="publication-description">
+          <p> {this.props.description} </p>
+          <div className="external-links">
+            <a href={this.props.liveApp}> {this.props.liveAppName} </a>
+          </div>
+        </div>
+        <PublicationModal modalDisplay={this.state.modalDisplay} closeModalCallback={this.closeModal} title={this.props.title} image={this.props.image} description={this.props.description} />
+      </div>
     );
   }
 });
--- app/assets/javascripts/components/content/publications.js.jsx
+++ app/assets/javascripts/components/content/publications.js.jsx
@@ -1,11 +1,9 @@
 var Publications = React.createClass({
   render: function() {
     return (
-      <div>
-        <Publication />
-        <Publication />
-        <Publication />
-        <Publication />
+      <div id="publications">
+        <Publication title="The Turing Test" description="I designed and built Game the Progress, which is a web app created to give teachers a platform to custom build and play game quizzes with multiple students. I worked with a team of four other developers over an eight day period to go from initial pitch to functional product. I worked primarily on the backend ActiveRecord associations and Action Cable WebSockets interface. With my team I demoed the app in front of a live audience and it is available to use on Heroku." image="https://media.licdn.com/media-proxy/ext?w=800&h=800&f=n&hash=PsHBYbLmA06UclvJK%2BlANZQAVqc%3D&ora=1%2CaFBCTXdkRmpGL2lvQUFBPQ%2CxAVta9Er0Vinkhwfjw8177yE41y87UNCVordEGXyD3u0qYrdf3Pof8SLebb3uVsTKiUclFZnLvL6STngD8a6Ioq6etxyisXgIY24ZxUBbFImi24" liveApp="http://gametheprogress.herokuapp.com/" liveAppName="Kindle Store" />
+
       </div>
     );
   }
