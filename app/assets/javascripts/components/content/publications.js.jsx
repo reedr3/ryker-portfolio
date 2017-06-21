3c790c91fdf759c5be4c0b31ab9a99498eb56ac3
Ryker Reed <ryker.reed3@gmail.com>
2017-06-21 13:57:24 -0700

    Adds more images for hobbies, publications, teaching.

--- app/assets/javascripts/components/content/publications.js.jsx
+++ app/assets/javascripts/components/content/publications.js.jsx
@@ -2,8 +2,7 @@ var Publications = React.createClass({
   render: function() {
     return (
       <div id="publications">
-        <Publication title="The Turing Test" description1="I wrote and published a science fiction novella. It won the MIT Louis Kampf Writing Prize in Women's and Gender Studies." description2="Sam is a college student who loves virtual reality. Alan is an android looking for a place in the world. After a chance meeting, a fast friendship blossoms between them. Even though much has advanced by the year 2040, the world is still not always kind to the ones it doesn’t understand. The two friends unearth many questions for themselves and for society, questions of what it means to be human, and what it means to be true to yourself." image="https://media.licdn.com/media-proxy/ext?w=800&h=800&f=n&hash=PsHBYbLmA06UclvJK%2BlANZQAVqc%3D&ora=1%2CaFBCTXdkRmpGL2lvQUFBPQ%2CxAVta9Er0Vinkhwfjw8177yE41y87UNCVordEGXyD3u0qYrdf3Pof8SLebb3uVsTKiUclFZnLvL6STngD8a6Ioq6etxyisXgIY24ZxUBbFImi24" liveApp="http://gametheprogress.herokuapp.com/" liveAppName="Kindle Store" />
-
+        <Publication title="The Turing Test" description1="I wrote and published a science fiction novella. It won the MIT Louis Kampf Writing Prize in Women's and Gender Studies." description2="Sam is a college student who loves virtual reality. Alan is an android looking for a place in the world. After a chance meeting, a fast friendship blossoms between them. Even though much has advanced by the year 2040, the world is still not always kind to the ones it doesn’t understand. The two friends unearth many questions for themselves and for society, questions of what it means to be human, and what it means to be true to yourself." image="https://s-media-cache-ak0.pinimg.com/736x/51/a6/a7/51a6a7af68bf9dd792645ceb248ebd68.jpg" liveApp="http://gametheprogress.herokuapp.com/" liveAppName="Kindle Store" />
       </div>
     );
   }
