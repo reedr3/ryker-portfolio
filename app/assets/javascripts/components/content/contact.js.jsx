ee6c8b745e1694041e5815ae20d035c1c0be8fc6
Ryker Reed <ryker.reed3@gmail.com>
2017-06-24 22:58:09 -0700

    Adds styling to contact form.

--- app/assets/javascripts/components/content/contact.js.jsx
+++ app/assets/javascripts/components/content/contact.js.jsx
@@ -1,14 +1,23 @@
 var Contact = React.createClass({
   render: function() {
     return (
-      <div>
+      <div id="contact">
         <h1>Contact Me!</h1>
         <form action='/contact_emails' method='post'>
           <input type='hidden' name='authenticity_token' value={this.props.authenticityToken} />
-          <input type='text' name='name' />
-          <input type='text' name='email' />
-          <input type='text' name='message' />
-          <input type='submit' value='Submit'/>
+          <div id='form-name'>
+            <label for='name'>Name: </label>
+            <input type='text' name='name' placeholder='Your Name' />
+          </div>
+          <div id='form-email'>
+            <label for='email'>Email: </label>
+            <input type='text' name='email' placeholder='Your Email' />
+          </div>
+          <div id='form-message'>
+            <label for='message'>Message: </label>
+            <textarea name='message' placeholder='Your Message' ></textarea>
+          </div>
+          <button type='submit'>Submit</button>
         </form>
       </div>
     );
