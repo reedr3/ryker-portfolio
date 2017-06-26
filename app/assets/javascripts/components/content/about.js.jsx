51e0a661b2876bddb2312969b7964fd2b94744b1
Ryker Reed <ryker.reed3@gmail.com>
2017-06-25 19:54:01 -0700

    Changes resume to hosted png image instead of pdf in public folder.

--- app/assets/javascripts/components/content/about.js.jsx
+++ app/assets/javascripts/components/content/about.js.jsx
@@ -6,7 +6,7 @@ var About = React.createClass({
         <p>I'm an education history component</p>
         <p>I'm some Ryker skills components</p>
         <p>I'm a resume component</p>
-        <a href="Ryker_Reed_Resume.pdf" download>Download Resume</a>
+        <a href="https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/19275059_10156202210868998_935256984192941727_n.jpg?oh=b7f94f9ccfed9a85af3608cb8f15bf92&oe=59DCACA6" download>Download Resume</a>
         <p>testimonial from maria</p>
       </div>
     );
