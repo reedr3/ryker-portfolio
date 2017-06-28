b2fe779a54bcf555a01c33ecaaa8fba32657a2ee
Ryker Reed <ryker.reed3@gmail.com>
2017-06-28 16:55:23 -0700

    Adds descriptions of my classes to the teaching page.

--- app/assets/javascripts/components/content/teaching.js.jsx
+++ app/assets/javascripts/components/content/teaching.js.jsx
@@ -4,21 +4,21 @@ var Teaching = React.createClass({
       <div id="teaching-experiences">
         <HobbyTeaching
           title="How to Be a Trans Ally"
-          description=""
+          description="I give workshops on how to be an ally to the trans community. The workshop focuses on how to make your school or workplace more inclusive of trans and gender non-conforming people as well as how to be a personal ally. I provide resources, share personal anecdotes, and lead interactive activities during the workshop designed to give participants more insight into the lives of trans and non-binary people and the unique challenges they can face. I am available to come give a workshop at your school, workplace, or organization, so feel free to get in touch through my contact page!"
           modalImages={["https://scontent.xx.fbcdn.net/v/t1.0-9/19420634_10156205034993998_1007789662130662136_n.jpg?oh=76b78630787198612f7bc196d766b88a&oe=59C6BC73", "https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/19400056_10156190015003998_3102385845901665293_n.jpg?oh=995490d99605856f967fc47b0049fb3a&oe=59E3B792", "https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/19396972_10156190014853998_569284778756724901_n.jpg?oh=d68e48845b05694a36cbd6c3f80ecf1d&oe=59CE83A5", "https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/19275155_10156190014923998_2207427040674911750_n.jpg?oh=5e24643b2558da35b72844e4dabaa60d&oe=59C7000E"]}
           imageCaptions={["Trans flag and symbol", "Page 1 of workshop handout. Definition of terms and examples of gender pronouns.", "Page 2 of workshop handout. Tips for allies.", "Page 3 of workshop handout. Gender Unicorn visual chart, credit: Trans Student Equality Resources. Design by Landyn Pan."]}
         />
 
         <HobbyTeaching
           title="Languages of Middle Earth"
-          description=""
+          description="I teach classes for middle and high schoolers about Middle Earth and its rich tapestry of languages. I created these classes because of my love of Linguistics and my love of Lord of the Rings. Tolkien himself was a linguist and imbued his world with an intricate linguistic history. I teach about the various languages seen throughout the books, as well as the evolution of the Elvish languages Sindarin and Quenya. I also cover two of Tolkiens invented alphabets, the Tengwar and the Angerthas."
           modalImages={["https://scontent.xx.fbcdn.net/v/t1.0-9/19554880_10156205035613998_6020797469397805803_n.jpg?oh=cfe508e79d8ef98e789952cea6187216&oe=59D9C2DC", "https://scontent.xx.fbcdn.net/v/t1.0-9/19424042_10156205085428998_6514918396494907282_n.jpg?oh=c6a213d86138f16ddf47f7ddfbbe6fe6&oe=59CCD97D", "https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/19274737_10156189942663998_3224191479949750665_n.jpg?oh=51ae485e06c70239064325b431a2a031&oe=59E7EE3F"]}
           imageCaptions={["Map of Middle Earth", "Page 1 of class handout. Shows alphabets and Elvish language evolutionary tree.", "Page 2 of class handout. Shows examples of Tolkien's different languages."]}
         />
 
         <HobbyTeaching
           title="Quirky Linguistics"
-          description=""
+          description="I teach classes for middle and high schoolers about Linguistics and all the weird and fascinating aspects of this wonderful field of study. I cover a variety of topics to give students an idea of what Linguistics is about and all the different subfields it covers. I teach them about everything from reduplication and infixation, to semantic satiation and the wug test. I also give them some interactive linguistic puzzles to solve using morpholigical analysis."
           modalImages={["https://scontent.xx.fbcdn.net/v/t1.0-9/19424478_10156205034558998_8934705063000381150_n.jpg?oh=26fb951d12fd3e24c11455588d89c282&oe=59D7F33C", "https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/19366611_10156190014988998_1056335471293014381_n.jpg?oh=9240ec95c20d398f6fdd57ccc5b1b31c&oe=59D31D1F", "https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/19260340_10156190014998998_5212705429059348326_n.jpg?oh=26782d86f4ea969c796b54698023a8c1&oe=59CE46A0", "https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/18403090_10156190014993998_3681553554698609889_n.jpg?oh=a30e8551988a3a20869f28771e2f4797&oe=59E72CEA"]}
           imageCaptions={["Famous linguistics experiment called the wug test.", "Page 1 of class handout.", "Page 2 of class handout.", "Page 3 of class handout."]}
         />
