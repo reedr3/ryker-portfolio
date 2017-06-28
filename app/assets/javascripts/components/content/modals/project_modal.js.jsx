95c81548a57f33066e04787eacfc8c7522a88b25
Ryker Reed <ryker.reed3@gmail.com>
2017-06-28 16:25:28 -0700

    Modifies project modal so that it only displays left and right arrows if theres more than one modal image.

--- app/assets/javascripts/components/content/modals/project_modal.js.jsx
+++ app/assets/javascripts/components/content/modals/project_modal.js.jsx
@@ -1,20 +1,40 @@
 var ProjectModal = React.createClass({
   render: function() {
-    return (
-      <div className="modal" style={this.props.modalDisplay}>
-        <span className="close" onClick={this.props.closeModalCallback}>&times;</span>
-        <span className="left" onClick={this.props.changeImageLeftCallback}>&lt;</span>
-        <span className="right" onClick={this.props.changeImageRightCallback}>&gt;</span>
-        <h2 className="p-modal-title"> {this.props.title} </h2>
-        <img className="p-modal-image" src={this.props.modalImages[this.props.currentModalImageIndex]} />
-        <div className="p-modal-image-caption"> {this.props.imageCaptions[this.props.currentModalImageIndex]} </div>
-        <div className="p-modal-text"> {this.props.description} </div>
-        <div className="p-modal-skills">
-          {this.props.skills.map(function(skill) {
+    // if there's more than one modal image, display left and right buttons
+    if (this.props.modalImages.length > 1) {
+      return (
+        <div className="modal" style={this.props.modalDisplay}>
+          <span className="close" onClick={this.props.closeModalCallback}>&times;</span>
+          <span className="left" onClick={this.props.changeImageLeftCallback}>&lt;</span>
+          <span className="right" onClick={this.props.changeImageRightCallback}>&gt;</span>
+          <h2 className="p-modal-title"> {this.props.title} </h2>
+          <img className="p-modal-image" src={this.props.modalImages[this.props.currentModalImageIndex]} />
+          <div className="p-modal-image-caption"> {this.props.imageCaptions[this.props.currentModalImageIndex]} </div>
+          <div className="p-modal-text"> {this.props.description} </div>
+          <div className="p-modal-skills">
+            {this.props.skills.map(function(skill) {
               return <p> {skill} </p>
             }.bind(this))}
+          </div>
         </div>
-      </div>
-    );
+      );
+    }
+    // if there's only one modal image, don't display left and right buttons
+    else if (this.props.modalImages.length == 1) {
+      return (
+        <div className="modal" style={this.props.modalDisplay}>
+          <span className="close" onClick={this.props.closeModalCallback}>&times;</span>
+          <h2 className="p-modal-title"> {this.props.title} </h2>
+          <img className="p-modal-image" src={this.props.modalImages[this.props.currentModalImageIndex]} />
+          <div className="p-modal-image-caption"> {this.props.imageCaptions[this.props.currentModalImageIndex]} </div>
+          <div className="p-modal-text"> {this.props.description} </div>
+          <div className="p-modal-skills">
+            {this.props.skills.map(function(skill) {
+              return <p> {skill} </p>
+            }.bind(this))}
+          </div>
+        </div>
+      );
+    }
   }
 });
