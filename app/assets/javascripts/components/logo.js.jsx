308929a8c9c5fb993b38912aa69fb5ae4683cdb9
Ryker Reed <ryker.reed3@gmail.com>
2017-09-15 23:55:53 -0400

    Adds link to logo to return to home of website, goes to about page.

--- app/assets/javascripts/components/logo.js.jsx
+++ app/assets/javascripts/components/logo.js.jsx
@@ -1,7 +1,7 @@
 var Logo = React.createClass({
   render: function() {
     return (
-      <img id="logo" src="https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/21728061_10156505665328998_5498779670418140584_n.jpg?oh=b2e71626e03fa2729df45e23efe13a76&oe=5A5DBBC9" />
+      <a href="http://ryker-reed.herokuapp.com"> <img id="logo" src="https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/21728061_10156505665328998_5498779670418140584_n.jpg?oh=b2e71626e03fa2729df45e23efe13a76&oe=5A5DBBC9"/> </a>
     );
   }
 });
