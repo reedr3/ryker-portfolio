b0c6ea7b05513fa7c88d2151584dc4185667607a
Ryker Reed <ryker.reed3@gmail.com>
2017-09-16 00:40:31 -0400

    Adds more formatting for testimonials. Adds formatting so text and image switch sides every other testimonial. Adds padding to intro text on about page.

--- app/assets/javascripts/components/content/about.js.jsx
+++ app/assets/javascripts/components/content/about.js.jsx
@@ -12,13 +12,14 @@ var About = React.createClass({
         </div>
 
         <div id="testimonials">
-          <h2>Testimonials</h2>
+          <h1>Testimonials</h1>
 
           <Testimonial
             name="Maria Uyeda"
             dialog="Ryker is an intelligent developer whose strength lies in his ability to solve complex problems through programming. He uses his vast understanding of algorithms and logic to creatively design products with a beautiful front-end and a clean back-end. I have worked on several teams with Ryker and witnessed his dedication for building meaningful programs that promote a more equal and just society. I would gladly work with Ryker again because of his programming expertise and his ability to bring a team together through strong leadership!"
             headshot="https://media.licdn.com/media/AAEAAQAAAAAAAAd9AAAAJDE0YzI2YzBiLTI1NGUtNDAzMy04MTMxLWFjNzI3NzU5YmQzMg.jpg"
             linkedin="https://www.linkedin.com/in/maria-uyeda/"
+            textSide="left"
           />
 
           <Testimonial
@@ -26,6 +27,7 @@ var About = React.createClass({
             dialog="I worked with Ryker on an intensive 2-week project when we were students at Dev Bootcamp. What impressed me most about Ryker was his ability to problem solve quickly and methodically. We were implementing a new feature in Rails (Action Cable) and he was able to read the limited documentation and get the feature working in a short period of time. Also, on our team, we had rotating project managers. When it was Ryker’s turn as manager, the team worked smoothly and communication was optimal. Everyone felt extremely confident in Ryker’s ability to lead our team. Ryker is intelligent, insightful, humble, and persistent. His work throughout the Dev Bootcamp program stood out as some of the best code of the group and he was always willing to explain difficult concepts to his cohort members. I would happily work on another project with Ryker in the future."
             headshot="https://media.licdn.com/media/AAEAAQAAAAAAAAcyAAAAJGFkMmVmZjc1LTk3MGItNGEyMS1iODRiLTMxNzQ2NGUwYmZjMQ.jpg"
             linkedin="https://www.linkedin.com/in/jimlengel/"
+            textSide="right"
           />
         </div>
 
