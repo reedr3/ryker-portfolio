8122561ed63d4e91c32e61ea20475ac0b91ed03e
Ryker Reed <ryker.reed3@gmail.com>
2017-09-21 23:21:28 -0400

    Adds turing test cover image and live link to kindle store. Adds some css formatting to publications page.

--- app/assets/javascripts/components/content/publications.js.jsx
+++ app/assets/javascripts/components/content/publications.js.jsx
@@ -5,11 +5,11 @@ var Publications = React.createClass({
         <Publication
           title="The Turing Test"
           description1="I wrote and published a science fiction novella. It won the MIT Louis Kampf Writing Prize in Women's and Gender Studies."
-          description2="Sam is a college student who loves virtual reality. Alan is an android looking for a place in the world. After a chance meeting, a fast friendship blossoms between them. Even though much has advanced by the year 2040, the world is still not always kind to the ones it doesn’t understand. The two friends unearth many questions for themselves and for society, questions of what it means to be human, and what it means to be true to yourself."
-          image="https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/21740033_10156505665333998_7377973128175305145_n.jpg?oh=32ddbe76d35f0e1f6e5a9f5508edf23e&oe=5A5761B7"
+          description2="Sam is a college student who loves virtual reality. Alan is an android looking for a place in the world. After a chance meeting, a fast friendship blossoms between them. Although much has advanced by the year 2040, the world is still not always kind to the ones it doesn’t understand. The two friends unearth many questions for themselves and for society, questions of what it means to be human, and what it means to be true to yourself."
+          image="https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/21765065_10156536735888998_3889393653312547547_n.jpg?oh=887ee516a209549046c506cb85e68e1b&oe=5A4519E7"
           imageCaption="Front Cover"
-          liveApp=""
-          liveAppName="Kindle Store - Coming Soon!"
+          liveApp="https://www.amazon.com/dp/B075TJS1YQ"
+          liveAppName="Kindle Store"
         />
       </div>
     );
