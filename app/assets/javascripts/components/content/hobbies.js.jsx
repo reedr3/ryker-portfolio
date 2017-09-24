041e10e3ec31d41959833a91d707fd95e67feced
Ryker Reed <ryker.reed3@gmail.com>
2017-09-24 15:20:27 -0400

    Updates image links on all pages.

--- app/assets/javascripts/components/content/hobbies.js.jsx
+++ app/assets/javascripts/components/content/hobbies.js.jsx
@@ -5,21 +5,21 @@ var Hobbies = React.createClass({
         <HobbyTeaching
           title="Barnstormers Softball"
           description="I play first base for the Barnstormers softball team. We are a team for trans and gender non-conforming players, who are so often excluded from sports. Along with our sibling team the Trailblazers we aim to make a place for trans players and make sports more welcoming to everyone. We play in the Boston Beantown league."
-          modalImages={["https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/19424450_10156204688203998_7388505682361865170_n.jpg?oh=5023f979ae50b5fd841a2090d9bb7a57&oe=5A3A7BF3", "https://scontent.xx.fbcdn.net/v/t1.0-9/19429975_10156204681498998_6473991747997450750_n.jpg?oh=016291af76dded0d984332df74ed58f7&oe=59D6CBEA", "https://scontent.xx.fbcdn.net/v/t1.0-9/19437659_10156204684953998_4328046667440108543_n.jpg?oh=1fa9a58b283206baf05a488a841a514d&oe=59DA4C25"]}
+          modalImages={["https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/19424450_10156204688203998_7388505682361865170_n.jpg?oh=5023f979ae50b5fd841a2090d9bb7a57&oe=5A3A7BF3", "https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/19429975_10156204681498998_6473991747997450750_n.jpg?oh=fb5aaba9997ccfc823b3da4bca61e87b&oe=5A4D72EA", "https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/19437659_10156204684953998_4328046667440108543_n.jpg?oh=323f323f8f594d31e0b7ce16a0d909d3&oe=5A50F325"]}
           imageCaptions={["Barnstormers and Trailblazers joint team photo (2016)", "Barnstormers celebrating a double header victory (2015)", "Ryker at bat (2016)"]}
         />
 
         <HobbyTeaching
           title="Constructed Languages"
           description="I have been fascinated by constructed languages since I was young. Tolkien and his extremely elaborate and extensive constructed languages has been a particular inspiration. Using my knowledge of Linguistics, I work on creating my own grammars and alphabets, as well as reading about constructed languages in literature and movies."
-          modalImages={["https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/21743277_10156501902428998_5801873539656788311_n.jpg?oh=ae9aa3641f1ff2267d39575b64a8e8d4&oe=5A131753", "https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/21740661_10156501902423998_2119303293922793447_n.jpg?oh=abfc5894e931c5a6bc43eaf8fc67bbaf&oe=5A5F6CDB", "https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/21751553_10156501902418998_6666268973380199923_n.jpg?oh=02c77b5c61ec368c1caa3734e4193702&oe=5A5B6E6C"]}
+          modalImages={["https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/21743277_10156501902428998_5801873539656788311_n.jpg?oh=2b0fbcab30b970bc1d81ea9610f0edcf&oe=5A3AA453", "https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/21740661_10156501902423998_2119303293922793447_n.jpg?oh=74ee477c70eb283e8b8c093df58d629b&oe=5A86F9DB", "https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/21751553_10156501902418998_6666268973380199923_n.jpg?oh=02c77b5c61ec368c1caa3734e4193702&oe=5A5B6E6C"]}
           imageCaptions={["A message written in my invented alphabet. Can you decipher it?", "A chart showing the consonants of my invented alphabet.", "A chart showing the vowels of my invented alphabet."]}
         />
 
         <HobbyTeaching
           title="Cosplay"
           description="I love making costumes and just this year I finally attended my first Convention. I enjoy doing solo cosplays and also group cosplays with my friends. My favorite things to make are props and I'm especially good at making all manner of things out of duct tape."
-          modalImages={["https://scontent.xx.fbcdn.net/v/t1.0-9/19420734_10156204842748998_215921509591975239_n.jpg?oh=93ee9c93904fedf49a5b24d400b4a0f5&oe=59D36CB5", "https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/19437598_10156204943868998_6577700759491729550_n.jpg?oh=c9d77188a3ed82157ffc6fa90979449c&oe=5A3B0E4B", "https://scontent.xx.fbcdn.net/v/t1.0-9/19510113_10156204833768998_2838069383390061927_n.jpg?oh=8f887028124d478f8b00cb1b2e232620&oe=59DC59DC"]}
+          modalImages={["https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/19420734_10156204842748998_215921509591975239_n.jpg?oh=1b8e120d86f8a3806a1aadab1054e2ff&oe=5A4A13B5", "https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/19437598_10156204943868998_6577700759491729550_n.jpg?oh=c9d77188a3ed82157ffc6fa90979449c&oe=5A3B0E4B", "https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/19510113_10156204833768998_2838069383390061927_n.jpg?oh=25f09f81fb7ac349730b29389711b323&oe=5A5300DC"]}
           imageCaptions={["Commander Riker and Lieutenant Geordi meeting Lieutenant Uhura (the incredible Nichelle Nichols)!", "Milo, Kida, and Audrey from Atlantis the Lost Empire.", "Rey and Finn from the Force Awakens."]}
         />
       </div>
--- app/assets/javascripts/components/content/projects.js.jsx
+++ app/assets/javascripts/components/content/projects.js.jsx
@@ -9,8 +9,8 @@ var Projects = React.createClass({
           liveApp="http://ryker-reed.herokuapp.com/"
           liveAppName="Heroku App"
           skills={["Ruby", "Rails", "JavaScript", "React", "HTML5", "CSS"]}
-          logoImage="https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/21462249_10156514832993998_1366816858867923594_n.jpg?oh=39f337c10a5c590f1b90b71e99c0be93&oe=5A5FFEAF"
-          modalImages={["https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/21462421_10156515180118998_5119511060757005911_n.jpg?oh=8d68ef8edfa8d68bf54374d562d29d5e&oe=5A5A637E", "https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/21766282_10156539082023998_1153086474798877555_n.jpg?oh=a34c253e5e9149b4d476a4511f3d08a0&oe=5A5CF9D7", "https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/21462941_10156515165628998_1524018878667669858_n.jpg?oh=3439772e715bb379c934e512e360af14&oe=5A550806", "https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/21739995_10156515165658998_8383588823318379725_n.jpg?oh=cd2ac05e8b39db3ec1243683dc3d2c8d&oe=5A41D86D", "https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/21731209_10156515165668998_3982420408126279535_n.jpg?oh=9674ddc2948183aaae0fef1a9c4ab645&oe=5A59D72A"]}
+          logoImage="https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/21462249_10156514832993998_1366816858867923594_n.jpg?oh=27e0e0bb49e87842d73a544b3bf1cd30&oe=5A878BAF"
+          modalImages={["https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/21462421_10156515180118998_5119511060757005911_n.jpg?oh=8d68ef8edfa8d68bf54374d562d29d5e&oe=5A5A637E", "https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/21766282_10156539082023998_1153086474798877555_n.jpg?oh=74780cc76cc418847fbc8afc416679b7&oe=5A8486D7", "https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/21462941_10156515165628998_1524018878667669858_n.jpg?oh=3439772e715bb379c934e512e360af14&oe=5A550806", "https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/21739995_10156515165658998_8383588823318379725_n.jpg?oh=cd2ac05e8b39db3ec1243683dc3d2c8d&oe=5A41D86D", "https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/21731209_10156515165668998_3982420408126279535_n.jpg?oh=9674ddc2948183aaae0fef1a9c4ab645&oe=5A59D72A"]}
           imageCaptions={["", "The About page.", "The Projects page.", "The Teaching page.", "The Hobbies page."]}
         />
 
@@ -22,7 +22,7 @@ var Projects = React.createClass({
           liveAppName="Heroku App"
           skills={["Ruby", "Rails", "Action Cable", "HTML5", "CSS"]}
           logoImage="https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/21731140_10156514867528998_6705568416102017004_n.jpg?oh=bb52c8f4be3fd5c6390660f4f0158474&oe=5A4931BD"
-          modalImages={["https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/21433121_10156514984853998_8516371410498667700_n.jpg?oh=a4e7000899627894ec738953aa0b9b21&oe=5A173D35", "https://scontent.xx.fbcdn.net/v/t1.0-9/19511050_10156204503273998_4583540318610076101_n.jpg?oh=b6e525773c0069012fa035b792a4b010&oe=59CD5EF4", "https://scontent.xx.fbcdn.net/v/t1.0-9/19420774_10156204503438998_1597688921241360681_n.jpg?oh=ca85c9a183bb7488502745d5dad778f6&oe=59DB8C99", "https://scontent.xx.fbcdn.net/v/t1.0-9/19554717_10156204503198998_3784043851095858786_n.jpg?oh=dbe7841be96cd2cadff3fb6c2f42a936&oe=59D4DAF0"]}
+          modalImages={["https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/21433121_10156514984853998_8516371410498667700_n.jpg?oh=51cdc1939de27d489ae440ce5eca91f2&oe=5A3ECA35", "https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/19511050_10156204503273998_4583540318610076101_n.jpg?oh=c2fb3ae5ee152e9f98fee00547ccdf9a&oe=5A4405F4", "https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/19420774_10156204503438998_1597688921241360681_n.jpg?oh=0c0fab901360e20249fc5942dd7bbe41&oe=5A523399", "https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/19554717_10156204503198998_3784043851095858786_n.jpg?oh=3ab9c97f59a1ed09b2ea73a36e76db70&oe=5A4B81F0"]}
           imageCaptions={["", "An example of a quiz question and multiple choice answers. This is the class view shown to students.", "The teacher's page, where they can access student results for past quizzes or begin a new quiz.", "The registration page, with options to sign up as a teacher or student."]}
         />
 
@@ -33,8 +33,8 @@ var Projects = React.createClass({
           liveApp="https://www.amazon.com/gp/product/B075FZGK3Y"
           liveAppName="Alexa Skill"
           skills={["JavaScript", "Node.js", "AWS Lambda"]}
-          logoImage="https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/21739951_10156515359463998_5371302477692199321_n.jpg?oh=2eae060814cab9c2ff6bc4a1224174f3&oe=5A5C9F3E"
-          modalImages={["https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/21752279_10156515359458998_9158730732599235780_n.jpg?oh=2ec2ed299d835b1fe30711b895b09598&oe=5A166FFC", "https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/21728027_10156502510253998_3300946840728705226_n.jpg?oh=ba6413adf8815926b168df0f5407d9e2&oe=5A550C91"]}
+          logoImage="https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/21739951_10156515359463998_5371302477692199321_n.jpg?oh=8d64431bfcb1b5c75f9f260f693c514c&oe=5A842C3E"
+          modalImages={["https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/21752279_10156515359458998_9158730732599235780_n.jpg?oh=3138253ac84993946a8c8e4f001e2d53&oe=5A3DFCFC", "https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/21728027_10156502510253998_3300946840728705226_n.jpg?oh=ba6413adf8815926b168df0f5407d9e2&oe=5A550C91"]}
           imageCaptions={["", "My Hogwarts Sorting Hat on the Alexa skills store, showing example invocation phrases."]}
         />
 
@@ -57,7 +57,7 @@ var Projects = React.createClass({
           liveApp="https://www.amazon.com/gp/product/B01MAWGFIG"
           liveAppName="Alexa Skill"
           skills={["JavaScript", "Node.js", "AWS Lambda"]}
-          logoImage="https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/21433057_10156515419303998_2105489843247705331_n.jpg?oh=9c59d9c8551f3c6d406c90cb128c21f8&oe=5A5CCE18"
+          logoImage="https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/21433057_10156515419303998_2105489843247705331_n.jpg?oh=a947c6274f886d75ea092d8e3d28e4d0&oe=5A845B18"
           modalImages={["https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/21433198_10156515419298998_7904922255306573446_n.jpg?oh=de80e492d60a37bfeda13aa1fc4c2a85&oe=5A5197D0", "https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/21728266_10156502510258998_1411791451728250120_n.jpg?oh=384e747da38dbf0b762cd4765207d544&oe=5A4B665A"]}
           imageCaptions={["", "My Linguistic Facts on the Alexa skills store, showing example invocation phrases."]}
         />
@@ -69,7 +69,7 @@ var Projects = React.createClass({
           liveApp="https://www.amazon.com/gp/product/B0736FC2LX"
           liveAppName="Alexa Skill"
           skills={["JavaScript", "Node.js", "AWS Lambda"]}
-          logoImage="https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/21462922_10156515475118998_5678893124895420001_n.jpg?oh=e802377dee2966e62d568056eb039d19&oe=5A5D3567"
+          logoImage="https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/21462922_10156515475118998_5678893124895420001_n.jpg?oh=4d15528aad6d66983288a3fa4477cf67&oe=5A84C267"
           modalImages={["https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/21731256_10156515469238998_7734718780192932514_n.jpg?oh=9cc5b65fa1a01deead24b39982fed4ea&oe=5A5894ED", "https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/21751808_10156502510303998_3076977622821457210_n.jpg?oh=05f2198aabe1a0e5eadb2c00f1ea90e4&oe=5A487E30"]}
           imageCaptions={["", "My Gender Identity Facts on the Alexa skills store, showing example invocation phrases."]}
         />
